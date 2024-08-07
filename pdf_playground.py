621a5f0b2221399d0e33afe050bf79ff285be39d
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-08-07 14:33:12 +0530

    Update pdf_playground.py

--- pdf_playground.py
+++ pdf_playground.py
@@ -1,9 +1,63 @@
+#---------------------------------------------------------------------------------------------------------------------------------
+### Authenticator
+#---------------------------------------------------------------------------------------------------------------------------------
 import streamlit as st
-import fitz  # PyMuPDF
+#---------------------------------------------------------------------------------------------------------------------------------
+### Import Libraries
+#---------------------------------------------------------------------------------------------------------------------------------
+from streamlit import session_state
+from streamlit.runtime.uploaded_file_manager import UploadedFile
+from streamlit_pdf_viewer import pdf_viewer
+#----------------------------------------
+import numpy as np
 import pandas as pd
-from PIL import Image
+import seaborn as sns
+import matplotlib.pyplot as plt
+#----------------------------------------
+import os
+import sys
+import traceback
 from io import BytesIO
+#----------------------------------------
+#import utils
+import re
 import base64
+import requests
+import contextlib
+from io import BytesIO
+from pathlib import Path
+from random import random
+from datetime import datetime
+from typing import Callable, Dict, Literal, Optional, Tuple, Union
+from io import BytesIO
+#----------------------------------------
+import fitz
+from PIL import Image
+from pypdf import PdfReader, PdfWriter, Transformation
+from pypdf.errors import PdfReadError, PdfStreamError
+
+#---------------------------------------------------------------------------------------------------------------------------------
+### Title and description for your Streamlit app
+#---------------------------------------------------------------------------------------------------------------------------------
+
+st.set_page_config(page_title="PDF Playground",
+                    layout="wide",
+                    page_icon="📄",            
+                    initial_sidebar_state="collapsed")
+#----------------------------------------
+st.title(f""":rainbow[PDF Playground | v0.1]""")
+st.markdown(
+    '''
+    Created by | <a href="mailto:avijit.mba18@gmail.com">Avijit Chakraborty</a>' |
+    for best view of the app, please **zoom-out** the browser to **75%**.
+    ''',
+    unsafe_allow_html=True)
+st.info('**An easy-to-use, open-source PDF application to preview and extract content and metadata from PDFs, add or remove passwords, modify, merge, convert and compress PDFs**', icon="ℹ️")
+#----------------------------------------
+
+#---------------------------------------------------------------------------------------------------------------------------------
+### Functions & Definitions
+#---------------------------------------------------------------------------------------------------------------------------------
 
 # Function to extract text and metadata from PDF
 def extract_pdf_info(pdf_file):
@@ -19,35 +73,23 @@ def pdf_page_to_image(doc, page_num):
     img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
     return img
 
-# Streamlit app setup
-st.set_page_config(page_title="PDF Uploader and Viewer", layout="wide")
-st.title("PDF Uploader and Viewer")
+#---------------------------------------------------------------------------------------------------------------------------------
+### Main app
+#---------------------------------------------------------------------------------------------------------------------------------
 
-# File uploader
 uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
 
-if uploaded_file is not None:
-    metadata, text, document = extract_pdf_info(uploaded_file)
-    
-    st.write("## PDF Metadata")
-    metadata_df = pd.DataFrame(list(metadata.items()), columns=["Key", "Value"])
-    st.table(metadata_df)
-    
-    st.write("## PDF Preview")
-    page_numbers = list(range(len(document)))
-    page_num = st.selectbox("Select Page Number", page_numbers)
-    
-    # Display selected page image
-    img = pdf_page_to_image(document, page_num)
-    st.image(img, caption=f"Page {page_num + 1}", use_column_width=True)
-    
-    st.write("## Extracted Text")
-    st.write(text[page_num])
+tab1, tab2, tab3, tab4, tab5  = st.tabs(["**Preview**","**Extract**","**Convert**","**Merge**","**Reduce**"])
 
-# Optional: Display all pages in a single view
-st.write("### View All Pages")
-for page_num in page_numbers:
-    st.write(f"### Page {page_num + 1}")
-    img = pdf_page_to_image(document, page_num)
-    st.image(img, caption=f"Page {page_num + 1}", use_column_width=True)
-    st.write(text[page_num])
+#---------------------------------------------------------------------------------------------------------------------------------
+### Content
+#---------------------------------------------------------------------------------------------------------------------------------
+
+with tab1:
+    
+    if uploaded_file is not None:
+        metadata, text, document = extract_pdf_info(uploaded_file)
+    
+        st.write("## PDF Metadata")
+        metadata_df = pd.DataFrame(list(metadata.items()), columns=["Key", "Value"])
+        st.table(metadata_df)
