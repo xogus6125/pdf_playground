6ca17d8bd6dc83ee77d3aa5fb8c651f609f31b0e
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-08-09 20:13:40 +0530

    Update pdf_playground.py

--- pdf_playground.py
+++ pdf_playground.py
@@ -71,6 +71,24 @@ def merge_pdfs(pdf_files):
     merged_pdf.seek(0)
     return merged_pdf
 
+#------------------------------------------------------------------------------------
+
+@st.cache_data(ttl="2h")
+def compress_pdf(input_pdf, output_pdf, compression_factor):
+    reader = PdfReader(input_pdf)
+    writer = PdfWriter()
+
+    for page in reader.pages:
+        page.compress_content_streams()
+        writer.add_page(page)
+
+    with open(output_pdf, 'wb') as f_out:
+        writer.write(f_out)
+
+@st.cache_data(ttl="2h")
+def pdf_to_images_bytes(pdf_bytes):
+    images = convert_from_bytes(pdf_bytes)
+    return images
 #---------------------------------------------------------------------------------------------------------------------------------
 ### Main app
 #---------------------------------------------------------------------------------------------------------------------------------
@@ -146,9 +164,58 @@ with tab3:
 ### Compress
 #---------------------------------------------------------------------------------------------------------------------------------
 
+with tab4:
 
+        st.markdown("This app allows you to reduce/compresss sizes of the PDF", unsafe_allow_html=True)  
+        uploaded_files = st.file_uploader("**Choose PDF file**", type="pdf", accept_multiple_files=True)
+        compression_factor = st.slider("**Select compression factor**", 0.1, 1.0, 0.5, 0.1)
+        st.divider()
 
+        if uploaded_files:
 
+            col1, col2 = st.columns((0.2,0.8))
+            with col1:
+
+                st.write(f"You have selected **{len(uploaded_files)} PDF file** for compress.")
+                if st.button("**Compress PDF**"):
+
+                        for uploaded_file in uploaded_files:
+                            temp_input_path = f"/tmp/{uploaded_file.name}"
+                            temp_output_path = f"compressed_{uploaded_file.name}"
+
+                            with open(temp_input_path, "wb") as f:
+                                f.write(uploaded_file.getbuffer())
+
+                            with st.spinner("Compressing PDFs..."):
+                                compress_pdf(temp_input_path, temp_output_path, compression_factor)
+
+                            with open(temp_output_path, "rb") as f:
+                                compressed_pdf = f.read()
+                            
+                            st.success("PDFs compressed successfully!")
+
+                            original_size = os.path.getsize(temp_input_path)
+                            compressed_size = os.path.getsize(temp_output_path)
+                            compression_ratio = (1 - compressed_size / original_size) * 100
+
+                            st.write(f"**Original PDF size of** : {original_size / 1024:.2f} KB")
+                            st.write(f"**Compressed PDF size of**: {compressed_size / 1024:.2f} KB")
+                            st.write(f"**Compression achieved**: {compression_ratio:.2f}%")
+
+                            st.download_button(label="**📥 Download Compressed PDF**",data=compressed_pdf,file_name="compressed_pdf.pdf",mime="application/pdf")
+
+                            with col2:
+
+                                st.subheader("**View : Compressed PDF**",divider='blue')    
+                                with st.container(height=600,border=True):
+
+                                    #compressed_pdf.seek(0)
+                                    images = pdf_to_images_bytes(compressed_pdf)
+                                    for page_num, img in enumerate(images):
+                                        st.image(img, caption=f"Page {page_num + 1}", use_column_width=True)
+
+                                    os.remove(temp_input_path)
+                                    os.remove(temp_output_path)  
 
 
 
