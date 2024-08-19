7ad07c63640fd830357aaf6ec25a029662d7151e
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-08-19 15:09:49 +0530

    Update pdf_playground.py

--- pdf_playground.py
+++ pdf_playground.py
@@ -36,12 +36,12 @@ st.set_page_config(page_title="PDF Playground | v0.1",
                     initial_sidebar_state="collapsed")
 #----------------------------------------
 st.title(f""":rainbow[PDF Playground]""")
-st.markdown(
-    '''
-    Created by | <a href="mailto:avijit.mba18@gmail.com">Avijit Chakraborty</a> ( :envelope: [Email](mailto:avijit.mba18@gmail.com) | :bust_in_silhouette: [LinkedIn](https://www.linkedin.com/in/avijit2403/) | :computer: [GitHub](https://github.com/DesolateTraveller) ) |
-    for best view of the app, please **zoom-out** the browser to **75%**.
-    ''',
-    unsafe_allow_html=True)
+#st.markdown(
+    #'''
+    #Created by | <a href="mailto:avijit.mba18@gmail.com">Avijit Chakraborty</a> ( :envelope: [Email](mailto:avijit.mba18@gmail.com) | :bust_in_silhouette: [LinkedIn](https://www.linkedin.com/in/avijit2403/) | :computer: [GitHub](https://github.com/DesolateTraveller) ) |
+    #for best view of the app, please **zoom-out** the browser to **75%**.
+    #''',
+    #unsafe_allow_html=True)
 st.info('**An easy-to-use, open-source PDF application to preview and extract content and metadata from PDFs, add or remove passwords, modify, merge, convert and compress PDFs**', icon="ℹ️")
 #----------------------------------------
 
