581cd8a1ce872a2472e8ddb14637e3f3e26f39bf
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-11-01 12:00:08 +0530

    Update pdf_playground.py

--- pdf_playground.py
+++ pdf_playground.py
@@ -43,7 +43,7 @@ st.title(f""":rainbow[PDF Playground]""")
     #for best view of the app, please **zoom-out** the browser to **75%**.
     #''',
     #unsafe_allow_html=True)
-st.info('**An easy-to-use, open-source PDF application to preview and extract content and metadata from PDFs, add or remove passwords, modify, merge, convert and compress PDFs**', icon="ℹ️")
+#st.info('**An easy-to-use, open-source PDF application to preview and extract content and metadata from PDFs, add or remove passwords, modify, merge, convert and compress PDFs**', icon="ℹ️")
 #----------------------------------------
 st.markdown(
     """
